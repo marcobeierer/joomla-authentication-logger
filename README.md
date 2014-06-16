cfe960765c05119593da4ee1798bfc3f53f261d0
marco <marco@webguerilla.net>
2014-06-16 15:26:47 +0200

    initial release

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 joomla-authentication-logger
 ============================
-
 An authentication logger for Joomla.
+
+## Logged Events
+- User login
+- User login failure
+- User logout
+- User logout failure
+- Password change (not yet implemented)
+- Password reset (not yet implemented)
+
+## What is logged?
+- Time
+- Severity
+- Action
+- Username
+- Details
+- User IP
+
+## Features
+- Log rotation: The plugin creates a new logfile every day.
+
+## Installation
+Just install the provided plugin package. The log files are created in your Joomla log directory.
---
+++ src/authenticationlogger.php
@@ -0,0 +1,59 @@
+<?php
+defined('_JEXEC') or die();
+jimport('joomla.log.log');
+
+class PlgSystemAuthenticationlogger extends JPlugin {
+
+	private function log(JLogEntry $logEntry) {
+
+		$date = JFactory::getDate()->format('Y-m-d');
+
+		JLog::addLogger(
+			array(
+				'text_file' => sprintf('authentication.%s.php', $date),
+				'text_entry_format' => '{TIME} | {PRIORITY} | {EVENT} | {USERNAME} | {MESSAGE} | {CLIENTIP}'
+			),
+			JLog::ALL,
+			array('authentication')
+		);
+
+		JLog::add($logEntry);
+	}
+
+	public function onUserLogin($response, $options) { // JAuthenticationResponse array
+
+		$logEntry = new JLogEntry('Successfull login.', JLog::INFO, 'authentication');
+		$logEntry->event = 'Login';
+		$logEntry->username = $response['username'];
+
+		$this->log($logEntry);
+	}
+
+	public function onUserLoginFailure($response) { // JAuthenticationResponse array
+
+		$logEntry = new JLogEntry($response['error_message'], JLog::WARNING, 'authentication');
+		$logEntry->event = 'Login';
+		$logEntry->username = $response['username'];
+
+		$this->log($logEntry);
+	}
+
+	public function onUserAfterLogout($options) {
+
+		$logEntry = new JLogEntry('Successfull logout.', JLog::INFO, 'authentication');
+		$logEntry->event = 'Logout';
+		$logEntry->username = $options['username'];
+
+		$this->log($logEntry);
+	}
+
+	public function onUserLogoutFailure($parameters) {
+
+		$logEntry = new JLogEntry('Erroneous logout.', JLog::WARNING, 'authentication');
+		$logEntry->event = 'Logout';
+		$logEntry->username = $parameters['username'];
+
+		$this->log($logEntry);
+	}
+
+}
