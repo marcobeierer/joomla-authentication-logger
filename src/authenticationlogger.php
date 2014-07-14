769fd261e6d4ad2d67ee7498f75d1d6de9f96fd5
marco <marco@webguerilla.net>
2014-07-14 11:14:55 +0200

    added license information to PHP files to make the component JED compliant

--- src/authenticationlogger.php
+++ src/authenticationlogger.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * @copyright  Copyright (C) 2014 Marco Beierer. All rights reserved.
+ * @license    http://www.gnu.org/licenses/agpl-3.0.html GNU/AGPL
+ */
+
 defined('_JEXEC') or die();
 
 class PlgSystemAuthenticationlogger extends JPlugin {
